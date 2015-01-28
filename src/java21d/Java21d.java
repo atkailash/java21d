e703053f46213fa1a493947874d06daacd29601e
Brian <kailash@calliope-u>
2015-01-27 17:34:09 -0700

    Day 01 finished

---
+++ src/java21d/Java21d.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package java21d;
+
+/**
+ *
+ * @author kailash
+ */
+public class Java21d {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
